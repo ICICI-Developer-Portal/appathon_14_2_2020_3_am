6cba59d49abc15d604ede395b1e60126ec9e2455
Gayathri Acharya <gayathri.acharya@niveussolutions.in>
2020-02-18 14:29:30 +0530

    rewrite code fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { BsModalService, BsModalRef } from 'ngx-bootstrap';
+import { Component, OnInit } from "@angular/core";
+import { BsModalService, BsModalRef } from "ngx-bootstrap";
 import {
   MatDialog,
   MatDialogRef,
-  MAT_DIALOG_DATA,
-} from '@angular/material/dialog';
-import { AdvertisementComponent } from './advertisement.component';
-import { Router } from '@angular/router';
+  MAT_DIALOG_DATA
+} from "@angular/material/dialog";
+import { AdvertisementComponent } from "./advertisement.component";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: "app-root",
+  templateUrl: "./app.component.html"
   //styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'ICICI';
+  title = "ICICI";
   modalRef: BsModalRef;
 
   constructor(public dialog: MatDialog, private router: Router) {}
 
   ngOnInit() {
     var url = this.router.url;
-    if (url !== '/index/termsandcondition') {
+    if (url !== "/index/termsandcondition") {
       const dialogRef = this.dialog.open(AdvertisementComponent, {
-        height: '600px',
-        width: '600px',
+        height: "600px",
+        width: "600px",
         disableClose: true,
-        panelClass: 'custom-modalbox'
+        panelClass: "custom-modalbox"
       });
     }
   }
