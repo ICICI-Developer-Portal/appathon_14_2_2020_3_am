681d8c76db0c408a49647f6cc45a9fe18be7cfa1
Gayathri Acharya <gayathri.acharya@niveussolutions.in>
2020-02-18 19:34:15 +0530

    download pdf added

--- src/app/maindashboard/onboardingrequests/onboardingrequests.component.ts
+++ src/app/maindashboard/onboardingrequests/onboardingrequests.component.ts
@@ -1,24 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { LoginService } from 'src/app/services';
-import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
+import { Component, OnInit } from "@angular/core";
+import { LoginService } from "src/app/services";
+import { Ng4LoadingSpinnerService } from "ng4-loading-spinner";
 // import 'rxjs/Rx';
-import { Http, Headers, Response } from '@angular/http';
-import { saveAs } from 'file-saver';
+import { Http, Headers, Response } from "@angular/http";
+import { saveAs } from "file-saver";
 //declare var require;
 
 @Component({
-  selector: 'app-onboardingrequests',
-  templateUrl: './onboardingrequests.component.html',
+  selector: "app-onboardingrequests",
+  templateUrl: "./onboardingrequests.component.html"
 })
 export class OnboardingrequestsComponent implements OnInit {
   dataSource: any;
-  p: any = '';
+  p: any = "";
   role: string;
   certificate: any;
   //showurl:Boolean;
   constructor(
     private adm: LoginService,
-    private spinnerService: Ng4LoadingSpinnerService,
+    private spinnerService: Ng4LoadingSpinnerService
   ) {
     this.request_data();
   }
@@ -45,16 +45,16 @@ export class OnboardingrequestsComponent implements OnInit {
   //supporting .sql,.cer (not supporting .png, .docx)
   downloadCertificate(url) {
     var json = {
-      filePath: url,
+      filePath: url
     };
 
-    var fileName = url.substring(url.lastIndexOf('/') + 1);
+    var fileName = url.substring(url.lastIndexOf("/") + 1);
 
     this.adm.downloadCertificate(json).subscribe((data: any) => {
       this.certificate = data._body;
       console.log(data._body);
       var blob = new Blob([this.certificate], {
-        type: 'text/plain',
+        type: "text/plain"
       });
       saveAs(blob, fileName);
     });
