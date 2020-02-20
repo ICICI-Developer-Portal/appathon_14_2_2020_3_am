23c4e83e2e9ee4456dc8c0ad2273e6a5bc4802f3
Gayathri Acharya <gayathri.acharya@niveussolutions.in>
2020-02-20 18:14:14 +0530

    code clean and download service change

--- config/application-constant.ts
+++ config/application-constant.ts
@@ -6,5 +6,62 @@ export const CONSTANTS = {
   URL: "Enter a valid URL",
   PDF_DOCUMENT: "Document",
   VIEW_FLOW: "View Flow",
-  PDF_TITLE: "Document"
+  PDF_TITLE: "Document",
+  DESKTOP_VIEW: "**This website is best viewed in desktop versions",
+  INTRODUCTION: "Introduction",
+  INTRODUCTION_DATA:
+    "ICICI Bank invites you to develop unique products using over 250 APIs hosted on this portal. The programme aims at creating next generation banking products to find some ground- breaking use cases on mobile and web space by attracting innovative and energetic startups. Participants need to envision a new solution and use the diverse sets APIs to create innovative minimum viable solutions. The solutions that surpass the currently available solutions in the market, give a superior and intuitive experience to the end user, will compete for the top prizes in the Grand Finale. The top three winners stand a chance to win prizes upto Rs.10 lakh along with a potential engagement opportunity with ICICI Bank. So get started and register now. ",
+  REGISTER_FOR_APPATHON: "Register for Appathon",
+  PROGRAM_DETAILS: "Program Details",
+  PROGRAM_DETAILS_SUBTEXT: "ICICI Bank Appathon will be conducted in 3 phases:",
+  ASSIGNMENT_IND: "assignment_ind",
+  PHASE_1: "Phase 1",
+  PHASE_1_DOC:
+    " Startups can register on https://developer.icicibank.com/ and submit their ideas by 23 February, 2020. The names of the shortlisted startups will be announced before February 27, 2020. Startups will have to share a brief presentation describing problem statement, market opportunity, product features, use cases and APIs to be used for integration.",
+  VOICE_CHAT: "voice_chat",
+  PHASE_2: "Phase 2",
+  PHASE_2_DOC:
+    "Shortlisted Startups will start working on actual product and integrations. Access to banks APIs would be given for product development. Startups would have to provide final submission of product demo by first week of March. In this round startups may also avail opportunity to discuss their solution with respective business and technology teams.",
+  LOCATION_CITY: "location_city",
+  PHASE_3: "Phase 3",
+  PHASE_3_DOC:
+    " Finalists will be announced mid-week of March 2020. They will be invited to Mumbai to present their solution in the ‘Grand Finale’ to be held during end of March.",
+  ELIGIBILTY_CRITERIA: "Eligibility Criteria",
+  ELIGIBILTY_CRITERIA_SUBTEXT:
+    "All participants must adhere to the following criteria to participate in the programme:",
+  ELIGIBILTY_CRITERIA_LS1: "This programme is open to startups.",
+  ELIGIBILTY_CRITERIA_LS2:
+    "This programme is also open to companies who have previously sold, licensed or otherwise provided services and/or similar solutions to ICICI Bank.",
+  ELIGIBILTY_CRITERIA_LS3:
+    "This is a team participation programme. You can have at most 5 members in a team.",
+  ELIGIBILTY_CRITERIA_LS4:
+    " In order to participate, the team needs to use at least one of the listed ICICI Bank APIs on the site. The theme of this Appathon is API Banking, so participants are recommended to use APIs as hosted on developer portal of ICICI Bank",
+  ELIGIBILTY_CRITERIA_LS5:
+    " By participating in the Appathon, you agree to the Terms and Conditions.",
+  PARAGRAPH:
+    "Since ICICI Bank is seeking innovative ideas and solutions, there is no single criterion on which we will judge your submission. Ideas will be looked at from multiple perspectives like viability, implementation ability, benefits, completeness of the solution, Business potential, relevance, user experience and scalability. following parameters can be used as an indicator of how the evaluation will happen to determine finalists and overall award recipients:",
+  PARAGRAPH_LS1:
+    "The judges reserve the right to provide a participant who applies for one category with an award from another category, at their sole discretion.",
+  PARAGRAPH_LS2:
+    "The decisions of the judges are final and binding. If we do not receive a sufficient number of entries meeting the entry requirements, we may, at our discretion, select fewer winners than described.",
+  PARAGRAPH_LS3:
+    "In the event of a tie between any eligible entries, an additional judge will break the tie based on the judging criteria described above.",
+  AWARDS: "Awards",
+  FIRST_PRIZE: "1st Prize",
+  FIRST_PRIZE_LS1: "INR 5 Lakh in monetary award",
+  FIRST_PRIZE_LS2:
+    "Potential opportunity to explore a commercial relationship with ICICI Bank",
+  SECOND_PRIZE: "2nd Prize",
+  SECOND_PRIZE_LS1: "INR 3 Lakh in monetary award",
+  SECOND_PRIZE_LS2:
+    "Potential opportunity to explore a commercial relationship with ICICI Bank",
+  THIRD_PRIZE: "3rd Prize",
+  THIRD_PRIZE_LS1: "INR 2 lakh in monetary award",
+  THIRD_PRIZE_LS2:
+    "Potential opportunity to explore a commercial relationship with ICICI Bank",
+  CONTACT_US: "Contact us",
+  CONTACT_US_LS1:
+    "For Programme related queries and support, please reach us at iciciappathon@icicibank.com",
+  CONTACT_US_LS2:
+    " For technology support related queries, please reach us at appathontech@icicibank.com"
 };
